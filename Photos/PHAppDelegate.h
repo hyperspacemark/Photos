4becf660b88260214598d5428c00403d59df05a8
Mark Adams <mark@thoughtbot.com>
2012-10-08 15:18:18 -0700

    Initial Commit

---
+++ Photos/PHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PHAppDelegate.h
+//  Photos
+//
+//  Created by Mark Adams on 10/8/12.
+//  Copyright (c) 2012 thoughtbot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Photos/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Photos
+//
+//  Created by Mark Adams on 10/8/12.
+//  Copyright (c) 2012 thoughtbot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PHAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PHAppDelegate class]));
+    }
+}
